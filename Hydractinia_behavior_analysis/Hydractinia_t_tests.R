06189c6e171d0939a812402c8c03f3f4a3f56e5c
Sydney Birch <31546643+sjb1061@users.noreply.github.com>
2022-11-18 10:08:41 -0500

    Update Hydractinia_t_tests.R

--- Hydractinia_behavior_analysis/Hydractinia_t_tests.R
+++ Hydractinia_behavior_analysis/Hydractinia_t_tests.R
@@ -2,11 +2,14 @@
      #A t-test for two independent samples without an assumption of equal variances
 
 #This script goes through the hydractinia planula larva behavior data where we placed larvae in a petri dish with a light stimulus underneath. 
-#We counted how many larvae were near the stimulus at the begining of the trial and how many larvae werre clustered within 1cm of the light stimulus after the trial. 
-#This script performs the t-test examiming this data and the generation of the behavior figure in the paper. 
+#We counted how many larvae were near the stimulus at the beginning of the trial and how many larvae werre clustered within 1cm of the light stimulus after the trial. 
+#This script performs the t-test examining this data and the generation of the behavior figure in the paper. 
 
 #The before numbers are the number of larvae within 1cm of the light before the experiement 
 #The after numbers are the number of larvae within 1cm of the light after the 8hr experiment
+
+
+##### T-tests for Before and After in each wavelength #####
 
 #Green 
 #tot # of larvae = 776
@@ -22,7 +25,6 @@ g_prop_after <- c(0.674699,0.108696,0.890052,0.916667,0.854651)
 t.test(g_prop_before, g_prop_after)
 #data:  g_prop_before and g_prop_after
 #t = -4.3302, df = 4.0367, p-value = 0.0121
-
 
 
 #Blue
@@ -41,7 +43,6 @@ t.test(b_prop_before, b_prop_after)
 #t = -4.7152, df = 5.0706, p-value = 0.005079
 
 
-
 #Red
 #tot # of larvae = 555
 r_before <- c(14,9,9)
@@ -56,6 +57,28 @@ r_prop_after <- c(0.078534,0.046875,0.063953)
 t.test(r_prop_before, r_prop_after)
 #data:  r_prop_before and r_prop_after
 #t = -0.46116, df = 3.9368, p-value = 0.669
+
+
+
+##### T-tests between wavelength - looking at the larval response (After values) #####
+
+#Green vs red
+t.test(g_after, r_after)
+#t = 3.0998, df = 4.0234, p-value = 0.03595
+t.test(g_prop_after, r_prop_after)
+#t = 4.1341, df = 4.0293, p-value = 0.01423
+
+#Blue vs Red
+t.test(b_after, r_after)
+#t = 5.2136, df = 5.3044, p-value = 0.002881
+t.test(b_prop_after, r_prop_after)
+#t = 4.3271, df = 5.0811, p-value = 0.007249
+
+#Green vs Blue
+t.test(g_after, b_after)
+#t = 1.4176, df = 4.7524, p-value = 0.2184
+t.test(g_prop_after, b_prop_after)
+#t = 1.0221, df = 7.2321, p-value = 0.3397
 
 
 
